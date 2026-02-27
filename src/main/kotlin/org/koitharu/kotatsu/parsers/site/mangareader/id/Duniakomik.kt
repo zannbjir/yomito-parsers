8d3dd8e3b6b35ac220497b38c94ea582820836b9
Razan Muhammad Ikhsan <raazan.muhammad.ikhsan@gmail.com>
2026-02-27 15:53:37 +0700

    Update Duniakomik.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/id/Duniakomik.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/id/Duniakomik.kt
@@ -7,9 +7,8 @@ import org.koitharu.kotatsu.parsers.model.ContentType
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
 
-@Broken
 @MangaSourceParser("DUNIAKOMIK", "DuniaKomik", "id", ContentType.HENTAI)
 internal class Duniakomik(context: MangaLoaderContext) :
-	MangaReaderParser(context, MangaParserSource.DUNIAKOMIK, "duniakomik.org", pageSize = 12, searchPageSize = 12) {
+	MangaReaderParser(context, MangaParserSource.DUNIAKOMIK, "duniakomik.com", pageSize = 12, searchPageSize = 12) {
 	override val datePattern = "MMM d, yyyy"
 }
