8467ee4b6dab63dc45b3d901da4823d1c818b124
Razan Muhammad Ikhsan <raazan.muhammad.ikhsan@gmail.com>
2026-02-27 15:50:26 +0700

    Update Manhwaku.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/id/Manhwaku.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/id/Manhwaku.kt
@@ -5,6 +5,7 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
 
+@Broken
 @MangaSourceParser("MANHWAKU", "Manhwaku", "id")
 internal class Manhwaku(context: MangaLoaderContext) :
 	MangaReaderParser(context, MangaParserSource.MANHWAKU, "manhwaku.id", pageSize = 20, searchPageSize = 10)
