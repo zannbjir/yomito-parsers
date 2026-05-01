c925018e7d7c3518a31f40f9745c0aa482e2439e
Razan Muhammad Ikhsan <raazan.muhammad.ikhsan@gmail.com>
2026-05-01 17:23:05 +0700

    Update UserAgents.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/network/UserAgents.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/network/UserAgents.kt
@@ -3,15 +3,25 @@ package org.koitharu.kotatsu.parsers.network
 public object UserAgents {
 
 	public const val CHROME_MOBILE: String =
-		"Mozilla/5.0 (Linux; Android 13) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.196 Mobile Safari/537.36"
+		"Mozilla/5.0 (Linux; Android 16) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Mobile Safari/537.36"
 
 	public const val FIREFOX_MOBILE: String =
-		"Mozilla/5.0 (Android 14; Mobile; LG-M255; rv:123.0) Gecko/123.0 Firefox/123.0"
+		"Mozilla/5.0 (Android 16; Mobile; LG-M255; rv:146.0) Gecko/146.0 Firefox/146.0"
+
+	/* Fallback: Set Linux as default for "DESKTOP" User-Agent */
 
 	public const val CHROME_DESKTOP: String =
-		"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+		"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.7444.59 Safari/537.36"
 
-	public const val FIREFOX_DESKTOP: String = "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0"
+	public const val FIREFOX_DESKTOP: String = "Mozilla/5.0 (X11; Linux x86_64; rv:145.0) Gecko/20100101 Firefox/145.0"
 
-	public const val KOTATSU: String = "Kotatsu/6.8 (Android 13;;; en)"
+	public const val CHROME_WINDOWS: String =
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36"
+
+	public const val FIREFOX_WINDOWS: String =
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:148.0) Gecko/20100101 Firefox/148.0"
+
+	public const val KOTATSU: String = "Kotatsu/9.0 (Android 16;;; en)"
+
+	public const val LEGACY_KOTATSU: String = "Kotatsu/6.8 (Android 13;;; en)"
 }
