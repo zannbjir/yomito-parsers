3396228aa38b6fb816210d281948091b3e3c00a8
Razan Muhammad Ikhsan <raazan.muhammad.ikhsan@gmail.com>
2026-05-10 14:10:08 +0700

    Add request headers to Apkomik parser

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/id/Apkomik.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/id/Apkomik.kt
@@ -1,5 +1,6 @@
 package org.koitharu.kotatsu.parsers.site.mangareader.id
 
+import okhttp3.Headers
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
@@ -9,5 +10,11 @@ import java.util.Locale
 @MangaSourceParser("APKOMIK", "Apkomik", "id")
 internal class Apkomik(context: MangaLoaderContext) :
 	MangaReaderParser(context, MangaParserSource.APKOMIK, "apkomik.cc", pageSize = 20, searchPageSize = 10) {
+	
 	override val sourceLocale: Locale = Locale.ENGLISH
+
+	override fun getRequestHeaders(): Headers = Headers.Builder()
+		.add("Referer", "https://$domain/")
+		.add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36")
+		.build()
 }
