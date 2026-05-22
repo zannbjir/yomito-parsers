34b11273f7f71a8a314573a96bb7a77262b398dd
Razan M. Ikhsan <raazan.muhammad.ikhsan@gmail.com>
2026-05-22 19:43:32 +0700

    Update MasterKomik.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/id/MasterKomik.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/id/MasterKomik.kt
@@ -1,10 +1,12 @@
 package org.koitharu.kotatsu.parsers.site.mangareader.id
 
+import org.koitharu.kotatsu.parsers.Broken
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
 
+@Broken
 @MangaSourceParser("MASTERKOMIK", "Tenshi", "id")
 internal class MasterKomik(context: MangaLoaderContext) :
 	MangaReaderParser(context, MangaParserSource.MASTERKOMIK, "tenshi01.id", pageSize = 20, searchPageSize = 20) {
