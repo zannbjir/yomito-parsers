531ab11673dcf1ab1604840d8033f931a366570f
Razan M. Ikhsan <raazan.muhammad.ikhsan@gmail.com>
2026-05-30 22:49:28 +0700

    Update AllPornComic.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/AllPornComic.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/AllPornComic.kt
@@ -9,6 +9,8 @@ import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 @MangaSourceParser("ALLPORN_COMIC", "AllPornComic", "en", ContentType.HENTAI)
 internal class AllPornComic(context: MangaLoaderContext) :
 	MadaraParser(context, MangaParserSource.ALLPORN_COMIC, "allporncomic.com", pageSize = 24) {
-	override val tagPrefix = "porncomic-genre/"
+	override val withoutAjax = true
+	override val listUrl = "porncomic/"
+	override val tagPrefix = "porncomic-cat/"
 	override val datePattern = "MMMM dd, yyyy"
 }
